a5fbbc5e921307f8b3aa490837c652f4fb4189a8
william-taylor <wi11berto@yahoo.co.uk>
2017-05-15 16:26:05 +0100

    adding type support + adding additional modules

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,18 @@
+
+import * as compression from 'compression';
+import * as bodyParser from 'body-parser';
 import * as express from 'express';
+import * as cors from 'cors';
 
 const app = express()
-
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.use(express.static(__dirname, { maxAge: '7d' }));
+app.use(bodyParser.json());
+app.use(compression());
+app.use(cors());
+app.get('/', (req, res) => {
+  res.send('Hello User!')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(8080, () => {
+  console.log('Ladywell Antiques Server Running!')
 })
