7b03fc65a97b3e54a04f91cc50b688d686291c1d
william-taylor <wi11berto@yahoo.co.uk>
2017-05-15 09:36:35 +0100

    basic todo app layout

---
+++ app/app/app.component.js
@@ -0,0 +1,26 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@angular/core");
+var app_model_1 = require("./app.model");
+var AppComponent = (function () {
+    function AppComponent() {
+        this.model = new app_model_1.Model();
+    }
+    AppComponent.prototype.getName = function () {
+        return this.model.user;
+    };
+    return AppComponent;
+}());
+AppComponent = __decorate([
+    core_1.Component({
+        selector: "todo-app",
+        templateUrl: 'app/app.component.html'
+    })
+], AppComponent);
+exports.AppComponent = AppComponent;
---
+++ app/app/app.component.ts
@@ -0,0 +1,15 @@
+
+import { Component } from "@angular/core";
+import { Model } from "./app.model";
+
+@Component({
+    selector: "todo-app",
+    templateUrl: 'app/app.component.html'
+})
+export class AppComponent {
+    model = new Model();
+
+    getName() {
+        return this.model.user;
+    }
+}
