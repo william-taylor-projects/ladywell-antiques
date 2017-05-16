602d230d457144676df4b51c3f39ec5ca5675f4d
william-taylor <wi11berto@yahoo.co.uk>
2017-05-16 04:14:29 +0100

    basic cover page put into componen

--- app/app/modules/app.modules.ts
+++ app/app/modules/app.modules.ts
@@ -2,12 +2,12 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
-import { AppComponent } from "../components/app.component";
+import { App } from "../components/app";
 
 @NgModule({
     imports: [BrowserModule, FormsModule],
-    declarations: [AppComponent],
-    bootstrap: [AppComponent]
+    declarations: [App],
+    bootstrap: [App]
 })
 export class AppModule {
 
