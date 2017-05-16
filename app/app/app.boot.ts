dc7eea488a58f050fb3143583ab804d798a833db
william-taylor <wi11berto@yahoo.co.uk>
2017-05-16 08:02:40 +0100

    routing

--- app/app/app.boot.ts
+++ app/app/app.boot.ts
@@ -3,7 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppModule } from './modules/app.modules';
 
-enableProdMode();
+//enableProdMode();
 
 let platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);
--- app/app/modules/app.modules.ts
+++ app/app/modules/app.modules.ts
@@ -2,10 +2,17 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
+import { RouterModule } from '@angular/router';
+
 import { App } from "../components/app";
 
+let routes = RouterModule.forChild([
+    { path: 'home', component: App },
+    {path: '**', redirectTo: 'home'}
+])
+
 @NgModule({
-    imports: [BrowserModule, FormsModule],
+    imports: [BrowserModule, FormsModule, routes],
     declarations: [App],
     bootstrap: [App]
 })
