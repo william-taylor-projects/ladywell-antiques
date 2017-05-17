924d141a4fd06b42556e809c060c0a3d47842002
william-taylor <wi11berto@yahoo.co.uk>
2017-05-17 09:23:30 +0100

    binding prod mode to localhost

--- app/app/app.boot.ts
+++ app/app/app.boot.ts
@@ -3,7 +3,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppModule } from './modules/app.module';
 
-//enableProdMode();
+if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
+    enableProdMode();
+}
 
 let platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);
