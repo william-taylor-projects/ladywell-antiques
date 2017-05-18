0df54c807737f0546db28b39d59496f21e32087b
william-taylor <wi11berto@yahoo.co.uk>
2017-05-18 16:25:21 +0100

    fixing build

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,8 +5,12 @@ import * as express from 'express';
 import * as cors from 'cors';
 import * as metrics from 'appmetrics-dash';
 
-const dash = metrics.monitor();
+const port = 8080;
 const app = express()
+const dash = metrics.monitor({
+    title: 'Ladywell Antiques Server',
+    port: 8000,
+});
 
 app.use(express.static(__dirname, { maxAge: '7d' }));
 app.use(bodyParser.json());
@@ -29,6 +33,6 @@ app.get('/', (req, res) => {
     res.send('Service Online')
 })
 
-app.listen(8080, () => {
-    console.log('Ladywell Antiques Server Running!')
+app.listen(port, () => {
+    console.log(`Running on port ${port}`)
 })
