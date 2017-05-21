e5da4f50d3826b9483ddd5219520a6a49743a926
william-taylor <wi11berto@yahoo.co.uk>
2017-05-21 12:27:45 +0100

    basic login work

--- app/app/components/login.component.ts
+++ app/app/components/login.component.ts
@@ -1,10 +1,17 @@
 
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
     selector: "login",
     templateUrl: 'app/components/login.component.html'
 })
 export class Login {
-    
+    constructor(private router: Router) {
+
+    }
+
+    login() {
+        this.router.navigate(['/admin']);
+    }
 }
