3606a065261ab46362ab558c721a6f0b80a85ca7
william-taylor <wi11berto@yahoo.co.uk>
2017-05-26 01:44:17 +0100

    adding docs url

--- server/app/boot.ts
+++ server/app/boot.ts
@@ -5,32 +5,34 @@ import * as express from 'express';
 import * as cors from 'cors';
 import * as metrics from 'appmetrics-dash';
 
+const dashboardPort = 8000;
 const port = 8080;
-const app = express()
 const dash = metrics.monitor({
-    title: 'Ladywell Antiques Server'//,
-    //port: 8000,
+    docs: 'https://github.com/freelance-william-taylor/ladywell-antiques',
+    title: 'Ladywell Antiques Server',
+    port: dashboardPort
 });
 
+const app = express();
 app.use(express.static(__dirname, { maxAge: '7d' }));
 app.use(bodyParser.json());
 app.use(compression());
 app.use(cors());
 
 app.get('/items', (req, res) => {
-    res.send('Dummy email items')
+    res.send('Dummy shop items')
 })
 
 app.get('/gallery', (req, res) => {
-    res.send('Dummy email gallery')
+    res.send('Dummy gallery items')
 })
 
 app.post('/email', (req, res) => {
-    res.send('Dummy email service')
+    res.send('Dummy email items')
 })
 
 app.get('/', (req, res) => {
-    res.send('Service Online')
+    res.redirect(`http://localhost:${dashboardPort}/appmetrics-dash`);
 })
 
 app.listen(port, () => {
