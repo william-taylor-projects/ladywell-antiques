977cf127ff416da387d4073084e7d2df65791604
william-taylor <wi11berto@yahoo.co.uk>
2017-05-26 01:56:33 +0100

    adding other routes + refactor

--- server/app/boot.ts
+++ server/app/boot.ts
@@ -5,6 +5,8 @@ import * as bodyParser from 'body-parser';
 import * as express from 'express';
 import * as cors from 'cors';
 
+import { galleryRoutes } from './router/gallery.router';
+import { emailRoutes } from './router/email.router';
 import { shopRoutes } from './router/shop.router';
 
 const dashboardPort = 8000;
@@ -15,17 +17,18 @@ const dash = metrics.monitor({
     port: dashboardPort
 });
 
+const redirect = `http://localhost:${dashboardPort}/appmetrics-dash`;
 const app = express();
+
 app.use(express.static(__dirname, { maxAge: '7d' }));
 app.use(bodyParser.json());
 app.use(compression());
 app.use(cors());
-
+app.use('/gallery', galleryRoutes);
+app.use('/email', emailRoutes);
 app.use('/shop', shopRoutes);
-
-
 app.get('/', (req, res) => {
-    res.redirect(`http://localhost:${dashboardPort}/appmetrics-dash`);
+    res.redirect(redirect);
 })
 
 app.listen(port, () => {
