507092e161c27e04f1e81eee6ccdb761ee9a0c53
william-taylor <wi11berto@yahoo.co.uk>
2017-07-07 03:04:07 +0100

    making sure css works...

--- app/app/components/app.component.ts
+++ app/app/components/app.component.ts
@@ -4,7 +4,8 @@ import { RouterModule } from '@angular/router';
 
 @Component({
     selector: "app",
-    templateUrl: 'app.component.html'
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css']
 })
 export class App {
 
