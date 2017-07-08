ba44aa331c1565f058e68b00ce965df926b1f311
william-taylor <wi11berto@yahoo.co.uk>
2017-07-08 12:27:13 +0100

    deployable website

--- app/app/boot-prod.ts
+++ app/app/boot-prod.ts
@@ -4,7 +4,10 @@ import "core-js/shim";
 import "zone.js/dist/zone";
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
 import { AppModule } from './modules/app.module';
+
+enableProdMode();
 
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,23 +1,59 @@
 
+const AotPlugin = require('@ngtools/webpack').AotPlugin;
 const webpack = require('webpack');
+const path = require('path');
+ 
+module.exports = options => {
+  const opt = options || {};
+  const config = {
+    entry: "./app/boot.ts",
+    output: {
+      path: __dirname + '/build',
+      filename: 'app.min.js'
+    },
+    resolve: {
+      extensions: ['.ts', '.js', '.html']
+    },
+    module: {
+      rules: [
+        { test: /\.css$/, loader: 'raw-loader' },
+        { test: /\.html$/, loader: 'raw-loader' }
+      ]
+    }
+  }
 
-module.exports = {
-  entry: "./app/boot.ts",
-  output: {
-    path: __dirname + '/build',
-    filename: 'app.min.js'
-  },
-  resolve: {
-    extensions: ['.ts', '.js', '.html']
-  },
-  module: {
-    rules: [
-      { test: /\.html$/, loader: 'raw-loader' },
-      { test: /\.css$/, loader: 'raw-loader' },
-      { test: /\.ts/, loaders: ['awesome-typescript-loader', 'angular2-template-loader'] }
-    ]
-  },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin()
-  ]
+  if (opt.MODE === 'prod') {
+    config.entry = "./app/boot-prod.ts";
+    config.module.rules.push({ test: /\.ts$/, loaders: ['@ngtools/webpack'] });
+    config.plugins = [
+      new AotPlugin({
+        tsConfigPath: './tsconfig.json',
+        entryModule: __dirname + '/app/modules/app.module#AppModule'
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        beautify: false,
+        mangle: {
+          screw_ie8: true,
+          keep_fnames: true
+        },
+        compress: {
+          warnings: false,
+          screw_ie8: true
+        },
+        comments: false
+      }),
+    ];
+  } else {
+    let rule = { 
+      test: /\.ts$/, 
+      loaders: [
+        'awesome-typescript-loader', 
+        'angular2-template-loader'
+      ] 
+    };
+    
+    config.module.rules.push(rule);
+  }
+
+  return config;
 }
